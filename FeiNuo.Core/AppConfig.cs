f6954912cbf304a971a96381b86c438026ff3008
wangjialiang <wjlxxfn@gmail.com>
2024-07-15 14:34:38 +0800

    添加项目顶层配置项FeiNuo

---
+++ FeiNuo.Core/AppConfig.cs
@@ -0,0 +1,29 @@
+﻿namespace FeiNuo.Core
+{
+    /// <summary>
+    /// 系统配置项
+    /// </summary>
+    public class AppConfig
+    {
+        /// <summary>
+        /// appsettings.json中的配置key
+        /// </summary>
+        public const string ConfigKey = "FeiNuo";
+
+        /// <summary>
+        /// 系统编码
+        /// </summary>
+        public string AppCode { get; set; } = "FeiNuo";
+
+        /// <summary>
+        /// 系统名称
+        /// </summary>
+        public string AppName { get; set; } = "菲诺";
+
+        /// <summary>
+        /// 版本号
+        /// </summary>
+        public string AppVersion { get; set; } = "1.0.0";
+
+    }
+}
