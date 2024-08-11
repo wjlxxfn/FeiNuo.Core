eb56a70088beac22ff5eeb17c85b61c3ad3e36bc
wangjialiang <wjlxxfn@gmail.com>
2024-08-11 10:38:02 +0800

    添加查询模型

---
+++ FeiNuo.Core/Data/QueryModel/BaseQuery.cs
@@ -0,0 +1,23 @@
+﻿namespace FeiNuo.Core
+{
+    /// <summary>
+    /// 基础的查询对象：包括一个时间段和模糊搜索查询
+    /// </summary>
+    public class BaseQuery
+    {
+        /// <summary>
+        /// 模糊搜索查询条件
+        /// </summary>
+        public string? Search { get; set; }
+
+        /// <summary>
+        /// 查询起始时间
+        /// </summary>
+        public DateOnly? StartDate { get; set; }
+
+        /// <summary>
+        /// 查询结束时间
+        /// </summary>
+        public DateOnly? EndDate { get; set; }
+    }
+}
